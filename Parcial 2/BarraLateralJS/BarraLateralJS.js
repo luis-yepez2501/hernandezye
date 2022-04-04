fba96866ca338ab738ffb64573dbe0a7feba7bce
luis-yepez2501 <yepezluis279@gmail.com>
2022-04-03 22:32:34 -0500

    Tareas

---
+++ Parcial 2/BarraLateralJS/BarraLateralJS.js
@@ -0,0 +1,9 @@
+
+document.getElementById("btn").addEventListener("click",alternarmenu)
+
+        function alternarmenu() {
+            
+            document.getElementById("bl").classList.toggle("Menu")
+
+            document.getElementById("m").classList.toggle("Margen")
+        }
