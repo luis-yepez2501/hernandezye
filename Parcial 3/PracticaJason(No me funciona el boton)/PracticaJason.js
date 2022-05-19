1ea7c0aa1a52bed8b3d0f3346d98e4a6cac13d58
luis-yepez2501 <yepezluis279@gmail.com>
2022-05-18 20:36:26 -0500

    Tareas

---
+++ Parcial 3/PracticaJason(No me funciona el boton)/PracticaJason.js
@@ -0,0 +1,52 @@
+$(document).ready(function (){
+    $('#botonAlerta').click(function() {
+        $('#ventanaAlerta').show();
+    });        
+
+
+    $('#botonCerrarAlerta').click(function() {
+        $('#ventanaAlerta').hide();
+    });
+
+    $('#botonJS').click(function() {
+        $('.modal-body').text("Esta seguro de comprar los boletos?");
+        $('#exampleModal').modal();
+    });
+
+    $('#botonSweet').click(function() {
+        swal({
+            title: "Compra realizada",
+            text: "Se compraron sus boletos de avion!",
+            icon: "success",
+
+        });
+    });
+
+    $('#botonAjax').click(function(){
+        var solicitud = new XMLHttpRequest();
+
+            solicitud.onreadystatechange = function() {   
+
+        if (solicitud.readyState == 4 && solicitud.status == 200) {
+        document.getElementById("encabezado").innerHTML = solicitud.responseText;
+        }};
+
+       solicitud.open("GET", "texto.txt", true);
+       solicitud.send();
+    });
+
+    $('#btnJson').click( function() {
+        $.post('getRegistro.PHP', {}, function(data){
+            console.log(data);
+            $('#num1').val(data.num1);
+            $('#num2').val(data.num2);
+            $('#num3').val(data.num3);
+            $('#num4').val(data.num4);
+            $('#num5').val(data.num5);
+            $('#num6').val(data.num6);
+
+        }, 'json')
+    });
+
+
+});
